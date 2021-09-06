377e9069718d24e2ad5d587cb85d16b4dbc9b5f7
Giovanny <glemay@adexos.fr>
2021-09-06 13:45:17 +0200

    Fix customer civility

---
+++ Model/Options.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adexos\Customweb\Model;
+
+class Options extends \Magento\Customer\Model\Options
+{
+    protected function _prepareNamePrefixSuffixOptions($options, $isOptional = false)
+    {
+        $options = trim($options);
+        if (empty($options)) {
+            return false;
+        }
+        $result = [];
+        $options = array_filter(explode(';', $options));
+        foreach ($options as $value) {
+            $value = $this->escaper->escapeHtml(trim($value));
+            $result[$value] = $value;
+        }
+        if ($isOptional && trim(current($options))) {
+            $result = array_merge([' ' => ' '], $result);
+        }
+
+        return $result;
+    }
+}
